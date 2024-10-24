d39e30b221084b720e5358066281b67da85e9b46
JEDO139191 <jedo139191@gmail.com>
2024-10-24 00:34:13 -0600

    Agregando Dependencias-Jonathan Dionicio

--- AppMascota/app/src/main/java/com/example/appmascota/Pantallas/adopcionMascota/MenuInicial.kt
+++ AppMascota/app/src/main/java/com/example/appmascota/Pantallas/adopcionMascota/MenuInicial.kt
@@ -1,13 +1,40 @@
 package com.example.appmascota.Pantallas.adopcionMascota
 
-import androidx.compose.foundation.layout.Column
+import android.annotation.SuppressLint
+import android.net.Uri
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-
-@Composable
-fun MenuInicial(navController: NavController) {
-    Column( ){
-        Text(text = "Menu Inicial")
-    }
-}
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import androidx.compose.foundation.layout.size
+import coil.compose.rememberImagePainter
